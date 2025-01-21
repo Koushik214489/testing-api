122c4bbc693b95bd888bf4477ce220a7fc1cbc3c
Koushik214489 <koushik.reddy.s70@kalvium.community>
2025-01-21 11:17:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+// API: Retrieve Students Above Threshold
+// ---------------------------------------
+// Task:
+// Implement an API to fetch students whose total marks exceed a given threshold.
+//
+// Endpoint:
+// POST /students/above-threshold
+//
+// Request Body:
+// {
+//   "threshold": <number>
+// }
+//
+// Response:
+// Success: List of students with their names and total marks who meet the criteria.
+// Example:
+// {
+//   "count": 2,
+//   "students": [
+//     { "name": "Alice Johnson", "total": 433 },
+//     { "name": "Bob Smith", "total": 410 }
+//   ]
+// }
+//
+// No Matches:
+// {
+//   "count": 0,
+//   "students": []
+// }
+//
+// Purpose:
+// Help teachers retrieve and analyze student performance efficiently.
+
+
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+app.use(bodyParse.json());
+const student = require('./data.json');
+app.post('/students/above-threshold',(req,res)=>{
+  const {threshold} = req.body;
+
+  if (typeof threshold !='number'){
+    return res.status(400).json({error:'Invalid threshold value.It mus be a number.'});
+
+  }
+  const filteredStudent = students.filter(student=> student.total > threshold);
+  const response = {
+    count:filteredStudents.length,
+    student:filteredStudents.map(({name,total}) => ([name,total])), 
+  }
+})
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
+
